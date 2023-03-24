49d0833e87cdc33a34385a62bc0a1f2896c92c5e
Pshenichka1 <116296628+Pshenichka1@users.noreply.github.com>
2023-03-24 17:42:09 +0000

    Deploying to gh-pages from @ Pshenichka1/goit-js-hw-09@b4d39209326a7b7c1e6ed8f636f9d4a6ad4be48e 🚀

---
+++ 01-color-switcher.d14f57b8.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n=["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"],r=null;function o(){return Math.floor(Math.random()*n.length)}t.addEventListener("click",(()=>{r=setInterval((()=>{let t=function(){let t="#";for(let e=0;e<6;e++)t+=n[o()];return t}();document.body.style.backgroundColor=t}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(()=>{clearInterval(r),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.d14f57b8.js.map
