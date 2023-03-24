12b537b6511534afd592eb041d9c66825b142f02
Pshenichka1 <pshenichka.olga@gmail.com>
2023-03-24 18:56:10 +0200

    задача 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+  let hex = [
+   "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F",];
+let timeId = null;
+// startBtn.setAttribute('disabled', false);
+
+startBtn.addEventListener('click', () => {
+  if( timeId = setInterval(() => {
+    let hexColor = generateHex();
+    document.body.style.backgroundColor = hexColor;
+  }, 1000)) {
+startBtn.setAttribute('disabled', true);
+  }     
+  
+}); 
+  stopBtn.addEventListener('click', () => {
+    if (clearInterval(timeId)) {
+      startBtn.disabled === false;
+  }
+});
+ 
+
+   function generateHex() {
+   let hexColor = "#";
+   for (let i = 0; i < 6; i++) {
+     hexColor += hex[getRandomHexColor()];
+    } return hexColor;
+ }
+   function getRandomHexColor() {
+    return Math.floor(Math.random() * hex.length);
+   }
+
 
